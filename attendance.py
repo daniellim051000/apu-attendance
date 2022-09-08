ba69955b8cf5d83b5a30733f925694b7b5812046
Daniel Lim Zheng Yu <daniel.lim@infrontconsulting.com.my>
2022-09-08 12:14:24 +0800

    update path and put driver

--- attendance.py
+++ attendance.py
@@ -9,8 +9,8 @@ import time
 
 from datetime import datetime
 
-username_1 = "####"
-password_1 = "####!"
+username_1 = "TP051131"
+password_1 = "C2rsd000D987!"
 username_2 = "####"
 password_2 = "####"
 
@@ -18,24 +18,24 @@ code = input("Enter the current OTP Code: ")
 
 def driver1():
 
-    driver = webdriver.Chrome(executable_path="#Ur Driver Path here")
+    driver = webdriver.Chrome(executable_path="chromedriver.exe")
     
     driver.get("https://apspace.apu.edu.my/tabs/dashboard")
            
     driver.implicitly_wait(5)
-    login_show = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[1]/ion-col[2]/ion-button")
+    login_show = driver.find_element("xpath","/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[1]/ion-col[2]/ion-button")
 
     login_show.click()
     
     driver.implicitly_wait(5)
     
-    AP_KEY = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[2]/ion-col[1]/form/div/div[1]/ion-input/input")
-    PASSWORD = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[2]/ion-col[1]/form/div/div[2]/ion-input/input")
+    AP_KEY = driver.find_element("xpath","/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[2]/ion-col[1]/form/div/div[1]/ion-input/input")
+    PASSWORD = driver.find_element("xpath","/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[2]/ion-col[1]/form/div/div[2]/ion-input/input")
     
     AP_KEY.send_keys(username_1)
     PASSWORD.send_keys(password_1)
     
-    login_button = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[2]/ion-col[1]/form/div/div[3]/ion-button")
+    login_button = driver.find_element("xpath","/html/body/app-root/ion-app/ion-router-outlet/app-login/ion-content/div/section[2]/div/ion-grid/ion-row[2]/ion-col[1]/form/div/div[3]/ion-button")
     
     login_button.click()
     
@@ -44,21 +44,21 @@ def driver1():
     
     ##### read username , tp , course
     
-    name = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-content/ion-tabs/div[1]/ion-router-outlet/app-dashboard/ion-content/div/ion-grid/ion-row/ion-col[1]/ion-card[2]/ion-card-header/ion-card-title")
+    name = driver.find_element("xpath","/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-content/ion-tabs/div[1]/ion-router-outlet/app-dashboard/ion-content/div/ion-row/ion-col[1]/ion-card[2]/ion-card-content/ion-row/ion-col[1]")
     # tp_number = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-content/ion-tabs/div[1]/ion-router-outlet/app-dashboard/ion-content/div/ion-grid/ion-row/ion-col[1]/ion-card[2]/ion-card-header/ion-card-subtitle")
     
-    attendance_page = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-content/ion-tabs/div[1]/ion-router-outlet/app-dashboard/ion-content/div/ion-grid/ion-row/ion-col[1]/ion-card[3]/ion-card-content/ion-grid/ion-row/ion-col[1]/ion-button")
+    attendance_page = driver.find_element("xpath","/html/body/app-root/ion-app/ion-router-outlet/app-tabs/ion-content/ion-tabs/div[1]/ion-router-outlet/app-dashboard/ion-content/div/ion-row/ion-col[1]/ion-card[2]/ion-card-content/ion-row/ion-col[1]")
     
     attendance_page.click()
     driver.implicitly_wait(5)
-    otp = driver.find_element_by_xpath("/html/body/app-root/ion-app/ion-router-outlet/app-student/ion-content/ion-grid/ion-row/ion-col/div/input[1]")
+    otp = driver.find_element("xpath","/html/body/app-root/ion-app/ion-router-outlet/app-student/ion-content/ion-grid/ion-row/ion-col/div/input[1]")
     otp.send_keys(code)
     
     driver.implicitly_wait(10)
     
     ##### alert message
     
-    message = driver.find_element_by_id("alert-1-msg").text
+    message = driver.find_element("xpath","alert-1-msg").text
     
     ### Show Time & Date
     now = datetime.now()
